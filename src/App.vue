02f45c13b67738282bab85dd08d4e17b436b63f6
minured <minured@qq.com>
2020-11-10 15:16:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- vue3不用根标签包裹 -->
+<img alt="Vue logo" src="./assets/logo.png" />
+<HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
